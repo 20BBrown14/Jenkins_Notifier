a0eeda5f2a2bc0c1cdd8851a1e53c8a84d388614
Brown,Branden <Branden.Brown2@cerner.com>
2019-02-11 09:56:52 -0600

    Replace buttons with icons and tooltips

--- src/modules/jobStatus.js
+++ src/modules/jobStatus.js
@@ -16,9 +16,11 @@ const NOT_BUILT = 'notbuilt';
 const NOT_BUILT_ANIME = 'notbuilt_anime';
 const YELLOW = 'yellow';
 const YELLOW_ANIME = 'yellow_anime';
+const ABORTED = 'aborted';
+const ABORTED_ANIME = 'aborted_anime';
 export const UNKNOWN_STATUS = 'unknown_status';
 
-const determineJobStatusImage = (color) => {
+export const determineJobStatusImage = (color) => {
   switch (color) {
     case BLUE:
       return successful;
@@ -28,8 +30,10 @@ const determineJobStatusImage = (color) => {
       return failed;
     case RED_ANIME:
       return failedAnime;
+    case ABORTED:
     case NOT_BUILT:
       return neverBuilt;
+    case ABORTED_ANIME:
     case NOT_BUILT_ANIME:
       return neverBuiltAnime;
     case YELLOW:
@@ -41,4 +45,25 @@ const determineJobStatusImage = (color) => {
   }
 };
 
-export default determineJobStatusImage;
+export const determineJobStatusTooltip = (color) => {
+  switch (color) {
+    case BLUE:
+      return 'Successful Build';
+    case YELLOW_ANIME:
+    case NOT_BUILT_ANIME:
+    case RED_ANIME:
+    case BLUE_ANIME:
+    case ABORTED_ANIME:
+      return 'Building';
+    case RED:
+      return 'Failed Build';
+    case NOT_BUILT:
+      return 'Never Built';
+    case YELLOW:
+      return 'Unstable Build';
+    case ABORTED:
+      return 'Aborted';
+    default:
+      return 'Unknown Status';
+  }
+};
